02d52cb972fc3864892fd3b2bc1832336c305db4
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-04-05 10:26:59 -0400

    closes #1

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <section id='home'>
       	<div className='intro'>
-		  	<h1>Hi, I'm Seamus.</h1>
+		  	  <h1>Hi, I'm Seamus.</h1>
       		<h1>I am a <span className='rainbow'>software engineer</span>.</h1>
       	</div>
     </section>

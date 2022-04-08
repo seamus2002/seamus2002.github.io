d3970a524880f874b2e6ac86c3b523f7b350dfb3
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-04-08 11:16:18 -0400

    added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../logo.png';
 
 const Navbar = () => {
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Seamus English</a>
+            <img src={logo} alt='logo' className='main-logo' />
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>

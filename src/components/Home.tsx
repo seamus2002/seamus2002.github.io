006e9f93c1bf4caffb6d40aeedaf2abf5b986639
seamus2002 <seamus.english58@myhunter.cuny.edu>
2022-04-09 21:25:53 -0400

    changed gradient text to rainbow text

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Home.css';
-import './gradientText.css';
+import './rainbowText.css';
 
 const Home = () => {
   return (
     <section id='home'>
       	<div className='intro'>
 		  	<h1><strong>Hello, I'm Seamus.</strong></h1>
-      		<h1><strong>I am a <span className='gradient-text'>software developer</span>.</strong></h1>
+      		<h1><strong>I am a <span className='rainbow'>software developer</span>.</strong></h1>
       	</div>
     </section>
   );

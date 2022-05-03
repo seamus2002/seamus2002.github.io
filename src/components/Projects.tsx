8fa9caaf14eb0d769114d2a2b42b84a53c6cd1c2
Seamus English <seamus.english58@myhunter.cuny.edu>
2022-05-03 11:57:57 -0400

    close #5

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,18 +7,20 @@ import projects from '../data/projects';
 const Projects = () => {
 	const createCard = (project: any) => {
 		return (
-			<div className='col d-flex'>
-				<ProjectCard
-					key={project.id}
-					name={project.name}
-					logo={project.logo}
-					description={project.description}
-					skills={project.skills}
-					liveLink={project.liveLink}
-					repoLink={project.repoLink}
-				/>
+			<div className="row flex-lg-row align-items-center g-5 py-5 text-start">
+				<div className="col-10 col-sm-8 col-lg-6">
+					<img src={ project.logo } className="d-block mx-lg-auto img-fluid" alt={ project.name } width="700" height="500" loading="lazy" />
+				</div>
+				<div className="col-lg-6">
+					<h3 className="fw-bold lh-1 mb-3">{ project.name }</h3>
+					<p className="lead">{ project.description }</p>
+					<div className="d-grid gap-2 d-md-flex justify-content-md-start">
+					<a href={ project.liveLink } target="_blank" rel="noopener noreferrer" className="btn btn-primary">Live Version</a>
+					<a href={ project.repoLink } target="_blank" rel="noopener noreferrer" className="btn btn-light">GitHub Repo</a>
+					</div>
+				</div>
 			</div>
-		)
+		);
 	}
 
 	return (

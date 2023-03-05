c199699cf5d3b386efff0fda99fe2014da4ab310
Seamus <theseamusenglish@gmail.com>
2023-03-04 19:15:49 -0500

    make main section responsive

--- src/containers/MainSection/MainSection.jsx
+++ src/containers/MainSection/MainSection.jsx
@@ -4,26 +4,28 @@ import "./MainSection.css";
 
 const MainSection = () => {
     return (
-        <section className="content">
-            <div className="header left">
-                <h2 className="title">Hello!</h2>
-                <h1 className="mainTitle">
-                    I'm <strong className="mainName">Seamus English</strong>
-                </h1>
-                <div style={{ paddingTop: 50 }}>
-                    <AppTypewriter
-                        strings={[
-                            "Full Stack Web Developer",
-                            "Front End Web Developer",
-                            "Continuously Learning",
-                        ]}
-                        wrapperClassName="typewriterWrapper"
-                        cursorClassName="typewriterCursor"
-                    />
+        <section className="container">
+            <div className="row flex-lg-row-reverse align-items-center py-9">
+                <div className="homeMainIcon col-10 col-sm-8 col-lg-6">
+                    <Laptop />
                 </div>
-            </div>
-            <div className="homeMainIcon right">
-                <Laptop />
+                <div className="header col-lg-6">
+                    <h2 className="title">Hello!</h2>
+                    <h1 className="mainTitle">
+                        I'm <strong className="mainName">Seamus English</strong>
+                    </h1>
+                    <div style={{ paddingTop: 50 }}>
+                        <AppTypewriter
+                            strings={[
+                                "Full Stack Web Developer",
+                                "Front End Web Developer",
+                                "Continuously Learning",
+                            ]}
+                            wrapperClassName="typewriterWrapper"
+                            cursorClassName="typewriterCursor"
+                        />
+                    </div>
+                </div>
             </div>
         </section>
     );

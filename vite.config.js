e97eeca06bc4dcefb2b64c5d462f28eda3d7b52a
Seamus <theseamusenglish@gmail.com>
2023-04-20 12:25:35 -0400

    update package.json

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    base: "/seamus2002.github.io/",
+    plugins: [react()],
+});

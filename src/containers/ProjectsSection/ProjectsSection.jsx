3e552f508828f9714df08785e546be0bf8ecf492
Seamus <theseamusenglish@gmail.com>
2023-04-20 12:48:25 -0400

    add menu mate project

--- src/containers/ProjectsSection/ProjectsSection.jsx
+++ src/containers/ProjectsSection/ProjectsSection.jsx
@@ -1,5 +1,6 @@
 import Project from "../../components/Project/Project";
 import "./ProjectsSection.css";
+import menu from "../../assets/menu.jpeg";
 import iss from "../../assets/iss.jpeg";
 
 const ProjectsSection = () => {
@@ -7,6 +8,18 @@ const ProjectsSection = () => {
         <section id="projects">
             <div className="container">
                 <h2 className="title text-center">My Projects</h2>
+                <Project
+                    name="Meun Mate"
+                    desc="This is a project I created with the Spoonacular API. 
+                    It allows the user to search for different types of recipes. 
+                    Once a recipe has been selected, users can view detailed 
+                    information about the recipe, including its ingredients, 
+                    nutrition information, and step-by-step instructions. I made 
+                    this website using React, Styled Components, and Framer Motion."
+                    icon={menu}
+                    liveLink="https://seamusenglish.com/menu-mate/"
+                    sourceLink="https://github.com/seamus2002/menu-mate"
+                />
                 <Project
                     name="International Space Station Tracker"
                     desc="This website displays the ISS's current lattitude,

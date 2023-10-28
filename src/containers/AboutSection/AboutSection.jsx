c007fa84f7567d54335a0befb83c536631271233
Seamus <theseamusenglish@gmail.com>
2023-10-28 09:09:48 -0400

    update bio

--- src/containers/AboutSection/AboutSection.jsx
+++ src/containers/AboutSection/AboutSection.jsx
@@ -2,44 +2,42 @@ import Person from "../../components/Person/Person";
 import "./AboutSection.css";
 
 const AboutSection = () => {
-    return (
-        <section id="about">
-            <div className="container">
-                <div className="row flex-lg-row align-items-center">
-                    <div className="about-icon col-10 col-sm-8 col-lg-6">
-                        <Person />
-                    </div>
-                    <div className="col-lg-6">
-                        <h2 className="title py-3">A little about myself</h2>
-                        <p className="lead py-2">
-                            Hello and welcome to my website! My name is Seamus,
-                            and I am a web developer in training. I am
-                            passionate about technology and the power it has to
-                            transform people's lives. I am committed to learning
-                            and growing every day, and I believe that with hard
-                            work and dedication, anything is possible.
-                        </p>
-                        <p className=" lead py-2">
-                            In addition to coding, I am passionate about gaming,
-                            traveling, and helping people, and I believe that my
-                            diverse interests and experiences help me approach
-                            problems from unique perspectives. I am a lifelong
-                            learner and enjoy staying up-to-date with the latest
-                            trends and advancements in the field.
-                        </p>
-                        <p className="lead py-2">
-                            My ultimate goal is to become a proficient full
-                            stack developer and work on projects that have a
-                            positive impact on people's lives. I am excited to
-                            continue learning and growing in this field, and I
-                            look forward to connecting with others who share my
-                            passion for technology.
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section id="about">
+      <div className="container">
+        <div className="row flex-lg-row align-items-center">
+          <div className="about-icon col-10 col-sm-8 col-lg-6">
+            <Person />
+          </div>
+          <div className="col-lg-6">
+            <h2 className="title py-3">A little about myself</h2>
+            <p className="lead py-2">
+              Hello! I'm Seamus, and I'm thrilled to share my journey into the
+              exciting world of data science and my aspirations for the future.
+              I am committed to learning and growing every day, and I believe
+              that with hard work and dedication, anything is possible.
+            </p>
+            <p className=" lead py-2">
+              My journey into data science began with a spark of interest during
+              my time as a web developer. While working on various web projects,
+              I couldn't help but notice the significance of data-driven
+              decisions in optimizing user experiences. The world of analytics
+              and data-driven insights opened up a new dimension of
+              understanding user behavior and system performance.
+            </p>
+            <p className="lead py-2">
+              In the next five years, my vision is to deepen my expertise in the
+              field of data science, with a strong focus on machine learning. I
+              see machine learning as the frontier of data science, where
+              data-driven insights evolve into predictive and prescriptive
+              models that can revolutionize industries and solve complex
+              problems.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default AboutSection;
--- src/containers/MainSection/MainSection.jsx
+++ src/containers/MainSection/MainSection.jsx
@@ -3,32 +3,32 @@ import Laptop from "../../components/Laptop/Laptop";
 import "./MainSection.css";
 
 const MainSection = () => {
-    return (
-        <section className="container min-vh-100 main-section">
-            <div className="row flex-lg-row-reverse align-items-center">
-                <div className="homeMainIcon col-10 col-sm-8 col-lg-6">
-                    <Laptop />
-                </div>
-                <div className="header col-lg-6">
-                    <h2 className="title">Hello!</h2>
-                    <h1 className="mainTitle">
-                        I'm <strong className="mainName">Seamus English</strong>
-                    </h1>
-                    <div style={{ paddingTop: 50 }}>
-                        <AppTypewriter
-                            strings={[
-                                "Full Stack Web Developer",
-                                "Front End Web Developer",
-                                "Continuously Learning",
-                            ]}
-                            wrapperClassName="typewriterWrapper"
-                            cursorClassName="typewriterCursor"
-                        />
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="container min-vh-100 main-section">
+      <div className="row flex-lg-row-reverse align-items-center">
+        <div className="homeMainIcon col-10 col-sm-8 col-lg-6">
+          <Laptop />
+        </div>
+        <div className="header col-lg-6">
+          <h2 className="title">Hello!</h2>
+          <h1 className="mainTitle">
+            I'm <strong className="mainName">Seamus English</strong>
+          </h1>
+          <div style={{ paddingTop: 50 }}>
+            <AppTypewriter
+              strings={[
+                "Data Scientist",
+                "Web Developer",
+                "Continuously Learning",
+              ]}
+              wrapperClassName="typewriterWrapper"
+              cursorClassName="typewriterCursor"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MainSection;
